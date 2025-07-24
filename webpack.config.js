7cfe3f715e9c2860f108c44e41b607ff21857a44
Jimmy Ho <jimmy.ho2017@gmail.com>
2025-07-24 19:55:01 -0400

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require ('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './client/index.js', 
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+        publicPath: '/',
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        historyApiFallback: true,
+        port: 8080,
+        open: true,
+        proxy: {
+            '/api': 'http://locahost:3000',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './client/index.html',
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+};
